ddc308cad0bb41886f6197e00300df28bc5fe40f
Daniel <danieljesus7k@gmail.com>
2024-12-20 14:31:35 -0300

    Repositório inicial

---
+++ Projeto/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Registro from './components/TelaRegistro/registro'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Registro />
+    </div>
+  )
+}
+
+export default App
