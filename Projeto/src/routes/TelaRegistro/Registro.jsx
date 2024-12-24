2909f18ccd0ee680af653f1334b209407216268d
Daniel <danieljesus7k@gmail.com>
2024-12-23 21:33:57 -0300

    Adaptando para utilizar os componentes

--- Projeto/src/routes/TelaRegistro/Registro.jsx
+++ Projeto/src/routes/TelaRegistro/Registro.jsx
@@ -1,89 +1,71 @@
-import React from 'react'
-import { useState } from 'react'
-import EastIcon from '@mui/icons-material/East';
-import "./Registro.css"
-
-//teste commit
+import React, { useState } from "react";
+import Formularios from "../../components/Formularios/Formularios";
+import ButtonSeta from "../../components/ButtonSeta/ButtonSeta";
+import LinkTelasEntrada from "../../components/LinkTelasEntrada/LinkTelasEntrada";
+import ImagemTelasInic from "../../components/ImagemTelasInic/ImagemTelasInic";
+import "./Registro.css";
+import imagemHeroina from "../../assets/imagem2.png"
 
 const Registro = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ nome, email, cpf, senha, confirmaSenha });
+  };
 
-    //Armazenar entradas do usuário
-    const [nome, setNome] = useState("");
-    const [email, setEmail] = useState("");
-    const [cpf, setCpf] = useState("");
-    const [senha, setSenha] = useState("");
-    const [confirmaSenha, setConfirmaSenha] = useState("");
-
-    //Função que chama quando o formulário é enviado
-    const handleSubmit = (event) => {
-        //Impedir da página recarregar
-        event.preventDefault();
-    }
   return (
     <div className="container">
-        <div className="blocoRegistro">
+      <div className="blocoRegistro">
         <h1 className="tituloRegistro">Registrar</h1>
         <form onSubmit={handleSubmit}>
-        <div className="conjuntoInputs">
-            <div className="inputNome">
-            <input 
-                type="text" 
-                placeholder="Nome de Usuário"
-                required
-                value = {nome}
-                onChange = {(e) => setNome(e.target.value)}
+          <div className="conjuntoInputs">
+            <Formularios
+              type="text"
+              placeholder="Nome de Usuário"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
             />
-            </div>
-            <div className="inputEmail">
-            <input 
-                type="email" 
-                placeholder="E-mail"
-                required
-                value = {email}
-                onChange = {(e) => setEmail(e.target.value)}
+            <Formularios
+              type="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            </div>
-            <div className="inputCpf">
-            <input 
-                type="text" 
-                placeholder="CPF"
-                required
-                value = {cpf}
-                onChange = {(e) => setCpf(e.target.value)}
+            <Formularios
+              type="text"
+              placeholder="CPF"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
             />
-            </div>
-            <div className="inputSenha">
-            <input 
-                type="password" 
-                placeholder="Senha"
-                required
-                value = {senha}
-                onChange = {(e) => setSenha(e.target.value)}
+            <Formularios
+              type="password"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
             />
-            </div>
-            <div className="inputConfirmaSenha">
-            <input 
-                type="password" 
-                placeholder="Confirmar Senha"
-                required
-                value = {confirmaSenha}
-                onChange = {(e) => setConfirmaSenha(e.target.value)}
+            <Formularios
+              type="password"
+              placeholder="Confirmar Senha"
+              value={confirmaSenha}
+              onChange={(e) => setConfirmaSenha(e.target.value)}
             />
-            </div>
-            <div className="botaoSeta">
-                <button className ="button" type = "submit"><EastIcon></EastIcon></button>
-            </div>
-            <div className="irParaLogin">
-                 <a href ="#">Fazer Login</a>
+            <ButtonSeta />
+            <LinkTelasEntrada
+              links={[
+                { href: "/login", text: "Fazer Login" },
+              ]}
+            />
+          </div>
+        </form>
+      </div>
+      <ImagemTelasInic image={imagemHeroina} />
+    </div>
+  );
+};
 
-        </div>
-            </div>
-                </form>
-                    </div>
-                        <div className="blocoImagem">  </div>
-                             </div>
-  )
-}
-
-export default Registro
+export default Registro;
