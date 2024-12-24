a3949af1e94df35cd173e597bfa0917eac58bd6d
Daniel <danieljesus7k@gmail.com>
2024-12-23 22:52:29 -0300

    Teste novas telas

--- Projeto/src/App.jsx
+++ Projeto/src/App.jsx
@@ -1,6 +1,9 @@
 
 import './App.css'
 import Registro from './routes/TelaRegistro/Registro'
+import TelaLogin from './routes/TelaLogin/TelaLogin'
+import RecuperarConta from './routes/TelaRecuperarConta/TelaRecuperarConta'
+import TelaRecuperarConta from './routes/TelaRecuperarConta/TelaRecuperarConta'
 
 function App() {
   
