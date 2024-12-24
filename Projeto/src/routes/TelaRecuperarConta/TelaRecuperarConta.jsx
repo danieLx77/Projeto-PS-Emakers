0fedb28d47c376425bf2d60071be5345caa4a23c
Daniel <danieljesus7k@gmail.com>
2024-12-23 22:53:35 -0300

    Tela Recuperar Conta utilizando componentes

---
+++ Projeto/src/routes/TelaRecuperarConta/TelaRecuperarConta.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Formularios from "../../components/Formularios/Formularios";  
+import ButtonSeta from "../../components/ButtonSeta/ButtonSeta";  
+import LinkTelasEntrada from "../../components/LinkTelasEntrada/LinkTelasEntrada";  
+import ImagemTelasInic from "../../components/ImagemTelasInic/ImagemTelasInic";  
+import imagemHeroina from "../../assets/imagem2.png";  
+import "./TelaRecuperarConta.css";  
+
+const TelaRecuperarConta = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ email, senha });
+  };
+
+  return (
+    <div className="containerRecConta">
+      <div className="blocoRecConta">
+        <h1 className="tituloRecConta">Recuperar Conta</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="conjuntoInputsRecConta">
+            <Formularios
+              type="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <ButtonSeta />
+            <div className="linksTelaRecConta">
+            <LinkTelasEntrada
+              links={[
+                { href: "/registro", text: "Criar conta" },
+                { href: "/login", text: "Fazer Login" },
+              ]}
+            />
+            </div>
+          </div>
+        </form>
+      </div>
+      <ImagemTelasInic image={imagemHeroina} />
+    </div>
+  );
+};
+
+export default TelaRecuperarConta;
