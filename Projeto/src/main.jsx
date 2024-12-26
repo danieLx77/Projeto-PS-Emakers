7a2a5eb6bfb65151cdfb3ca31679284ea0ba4da4
Daniel <danieljesus7k@gmail.com>
2024-12-26 17:41:16 -0300

    Adicionando rotas com React Router

--- Projeto/src/main.jsx
+++ Projeto/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+import TelaLogin from "./routes/TelaLogin/TelaLogin"
+import TelaRegistro from "./routes/TelaRegistro/Registro"
+import TelaRecuperarConta from "./routes/TelaRecuperarConta/TelaRecuperarConta"
+const router = createBrowserRouter([
+  {
+    path: "/Registrar",
+    element: <TelaRegistro />,
+  },
+  {
+    path: "/Login",
+    element: <TelaLogin />,
+  },
+  {
+    path: "/RecuperarConta",
+    element: <TelaRecuperarConta />,
+  },
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
