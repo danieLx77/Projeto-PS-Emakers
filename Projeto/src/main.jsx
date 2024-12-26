55fea8a7fa31e4d093e85600fafb60bf9a1543a8
Daniel <danieljesus7k@gmail.com>
2024-12-26 18:05:19 -0300

    Adicionando a tela de erro as rotas

--- Projeto/src/main.jsx
+++ Projeto/src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
 import TelaLogin from "./routes/TelaLogin/TelaLogin"
 import TelaRegistro from "./routes/TelaRegistro/Registro"
 import TelaRecuperarConta from "./routes/TelaRecuperarConta/TelaRecuperarConta"
-
+import TelaErro from "./routes/TelaErro/TelaErro"
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <TelaErro />,
     children : [
       {
         path: "/",
