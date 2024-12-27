0594475ebd2b5bc3710633c7e016343634485d5a
Daniel <danieljesus7k@gmail.com>
2024-12-27 19:59:06 -0300

    Retirando imports inúteis

--- Projeto/src/App.jsx
+++ Projeto/src/App.jsx
@@ -1,9 +1,6 @@
 
 import './App.css'
 import { Outlet } from "react-router-dom"
-import Registro from './routes/TelaRegistro/Registro'
-import TelaLogin from './routes/TelaLogin/TelaLogin'
-import TelaRecuperarConta from './routes/TelaRecuperarConta/TelaRecuperarConta'
 
 function App() {
   
